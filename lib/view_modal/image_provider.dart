dcfd4bb6e133c7db5e7116fb7015ad171da66cd1
Piyush Dewangan <150065893+piyush-143@users.noreply.github.com>
2025-07-29 11:39:19 +0530

    Img provider added

---
+++ lib/view_modal/image_provider.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImgProvider with ChangeNotifier {
+  String _imagePath = '';
+  String get imagePath => _imagePath;
+  File? _image;
+  File? get image => _image;
+  Future<void> pickImage({required ImageSource source}) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: source);
+    _imagePath = image!.path;
+    _image = File(_imagePath);
+    notifyListeners();
+  }
+}
